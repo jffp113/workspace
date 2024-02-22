9d74490d850d8f3a10fcbdb0bdb0ed7db26b9081
Jorge Pereira <jff.pereira@campus.fct.unl.pt>
2024-02-22 19:25:34 +0000

    Update git_shortcuts.sh

--- git_shortcuts.sh
+++ git_shortcuts.sh
@@ -12,3 +12,21 @@ git log -L 30,100 /file
 
 # detect code movements
 git blame -w -C
+
+# Log of actions in git
+git reflog
+
+# Word diff
+git diff --word-diff
+
+# Smart git conflict resolution
+git config --global rerere.enabled true
+# git branch in a column 
+git config --global column.ui auto
+# sort branch by commitr date
+git config --global branch.sort -committerdate
+
+git config gpg.format ssh
+git config user.signingkey ~/.ssh/key.pub
+
+
